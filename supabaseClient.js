7868b2be7b26a1ffd06314bdd7b961391357a8cc
Wahyu5677 <taqdirwahyu546@gmail.com>
2026-01-24 21:32:46 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.49.1/+esm';
 
 const SUPABASE_URL = 'https://uxitydifucsxupzckbto.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4aXR5ZGlmdWNzeHVwemNrYnRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNzEzOTEsImV4cCI6MjA4MTY0NzM5MX0.vGpjGHQdGqGJAbE1W3S-RnKInxu_OCS30r9us3v4WLM';
